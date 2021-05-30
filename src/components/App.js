456d6c403f8cd45364e2e2fd75eb7fce2d299d73
subhrajyoti <subhrajyotisankarsau@gmail.com>
2021-05-30 22:34:08 +0530

    initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,12 @@ import React from "react";
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
+
   render() {
     return  (
-      <div><SearchBar/></div>
+      <div className="ui container">
+        <SearchBar/>
+        </div>
     );
   }
 }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,8 +1,30 @@
 import React from "react";
 
 class SearchBar extends React.Component {
+  state = { term: "" };
+  onInputChange = (e) => {
+    this.setState({ term: e.target.value });
+  };
+  onFormSubmit = (e) => {
+    e.preventDefault();
+    //make sure we call callback from parent component
+  };
+
   render() {
-    return <div>SearchBar</div>;
+    return (
+      <div className="search-bar ui segment">
+        <form onSubmit={this.onFormSubmit} className="ui form">
+          <div className="field">
+            <label>Video Search</label>
+            <input
+              type="text"
+              onChange={this.onInputChange}
+              value={this.state.term}
+            />
+          </div>
+        </form>
+      </div>
+    );
   }
 }
 
