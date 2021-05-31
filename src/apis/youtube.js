8969ae77f14b43a45cfcb676e5ff5b7583f89ae5
subhrajyoti <subhrajyotisankarsau@gmail.com>
2021-06-01 00:19:04 +0530

    final commit

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,1 +1,13 @@
-const KEY = 'API_KEY';
+import axios from 'axios';
+
+const KEY = 'AIzaSyBll748D41ScuBRfElqPDCN6r8qYWGoF5s';
+
+export default axios.create({
+   baseURL:'https://www.googleapis.com/youtube/v3',
+   params:{
+       part:'snippet',
+       type:'video',
+       maxResult:5,
+       key:KEY
+   }
+})
---
+++ src/components/VideoItems.js
@@ -0,0 +1,18 @@
+import './VideoItem.css';
+import React from 'react';
+
+
+
+const VideoItem = (props) =>{
+    return (
+    <div onClick={() => props.onVideoSelect(props.video)} className="video-item item">
+        <img className="ui image" src={props.video.snippet.thumbnails.medium.url} alt={props.video.snippet.title}/>
+        <div className="content">
+            <div className="header">{props.video.snippet.title}</div> 
+        </div>      
+    </div>
+    );
+};
+
+
+export default VideoItem;
